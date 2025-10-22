4583a20cd8463d2aef1836dfacd763b25c787d8c
Andrew Bates <abates@omeganetserv.com>
2025-10-22 09:48:39 -0400

    fix:Added job imports to package

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -3,5 +3,12 @@
 This package includes a number of useful jobs to help manage Nautobot.
 """
 
-from .device_component_update import *
+from .device_component_update import DeviceComponentUpdate, DeviceComponentUpdateButton
+from .device_names import UpdateDeviceNames, UpdateDeviceNamesButton
 
+__all__ = [
+    "DeviceComponentUpdate",
+    "DeviceComponentUpdateButton",
+    "UpdateDeviceNames",
+    "UpdateDeviceNamesButton",
+]
